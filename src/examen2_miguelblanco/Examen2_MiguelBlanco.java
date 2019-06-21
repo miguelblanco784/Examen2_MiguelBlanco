45c5243926430282cd2c9c612f175871feb1032d
miguelblanco784 <miguelrobertoblanco@gmail.com>
2019-06-21 13:35:58 -0600

    Inicio Del Examen

--- src/examen2_miguelblanco/Examen2_MiguelBlanco.java
+++ src/examen2_miguelblanco/Examen2_MiguelBlanco.java
@@ -16,6 +16,7 @@ public class Examen2_MiguelBlanco {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("test");
     }
     
 }
